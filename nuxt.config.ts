858492ae7711d7fdaed721c8cd7af75130909628
abdobenali <abdobenali.dev@gmail.com>
2023-12-13 02:19:44 +0100

    chore: add dependencies and utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   css: ["~/assets/scss/global.scss"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "nuxt-rating",
+  ],
   devtools: { enabled: true },
+  googleFonts: {
+    families: {
+      "Jakarta+Plus+Sans": [400, 500, 600, 700],
+    },
+    display: "swap",
+    prefetch: true,
+  },
 });
